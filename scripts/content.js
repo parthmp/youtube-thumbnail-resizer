37f81ad7e5f6e26d326fbe3a69db9c46ae33c200
parthpmp <parthparmaronline@gmail.com>
2026-04-11 11:17:50 +0530

    added functionality to toggle shorts area on youtube home page

--- scripts/content.js
+++ scripts/content.js
@@ -16,9 +16,32 @@ function applyColumns(columns) {
     `;
 }
 
+function toggleShorts(hide) {
+
+	const styleId = "show-shorts";
+	let style = document.getElementById(styleId);
+
+	if (!style) {
+		style = document.createElement("style");
+		style.id = styleId;
+		document.head.appendChild(style);
+	}
+
+	let showShortsDisplay = hide ? "none" : "block";
+
+	style.textContent = `
+		ytd-rich-section-renderer {
+			display: ${showShortsDisplay} !important;
+		}`;
+
+}
+
 function init() {
     chrome.storage.sync.get(["columns"], (result) => {
         applyColumns(result.columns || 6);
+    });
+	chrome.storage.sync.get(["showShorts"], (result) => {
+        toggleShorts(result.showShorts);
     });
 }
 
@@ -28,6 +51,9 @@ chrome.storage.onChanged.addListener((changes) => {
     if (changes.columns) {
         applyColumns(changes.columns.newValue);
     }
+	if(changes.showShorts){
+		toggleShorts(changes.showShorts.newValue);
+	}
 });
 
 document.addEventListener("yt-navigate-finish", init);
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,10 +1,14 @@
 const slider = document.getElementById("slider");
 const value = document.getElementById("value");
+const shortsCheckbox = document.getElementById("shorts-checkbox");
 
 chrome.storage.sync.get(["columns"], (result) => {
     const cols = result.columns || 6;
     slider.value = cols;
     value.textContent = cols;
+});
+chrome.storage.sync.get(["showShorts"], (result) => {
+    shortsCheckbox.checked = result.showShorts;
 });
 
 slider.addEventListener("input", () => {
@@ -15,5 +19,11 @@ slider.addEventListener("input", () => {
 
     chrome.storage.sync.set({
         columns: cols
+    });
+});
+
+shortsCheckbox.addEventListener("change", () => {
+	chrome.storage.sync.set({
+        showShorts: shortsCheckbox.checked
     });
 });
